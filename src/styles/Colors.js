5f0cc4b8ce5aa68f74071d4919ae65cd8a4b909a
Rishen-Tharusha <rishen.enrich@gmail.com>
2024-09-06 17:21:05 +0530

    Setup

---
+++ src/styles/Colors.js
@@ -0,0 +1,14 @@
+const Colors = {
+    BLACK: '#000000',
+    WHITE: '#FFFFFF',
+    LIGHTGREY: '#F0F5FD',
+    YELLOW: '#FFA500',
+    PEACH: '#BA895D',
+    ORANGE: '#EE5921',
+    GREEN: '#00D90F',
+    PINK: '#FF004C',
+    GREY: '#7C8087',
+    DARKBLUE: '#102636'
+}
+
+export default Colors;
